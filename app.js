ee7a749a3dff5568983951f29b61f6b82d59745b
jc <vonzo55@gmail.com>
2015-08-13 02:32:24 -0500

    route fixed

--- app.js
+++ app.js
@@ -13,29 +13,6 @@ var route = {
   }
 };
 
-route.for('GET', '/search', function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write("Halo");
-  res.end();
-});
-
-route.for('POST', '/test', function(req, res) {
-  var incomming = "";
-  req.on('data', function(chunk) {
-    incomming += chunk.toString();
-  });
-
-  req.on('end', function() {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write(incomming);
-    res.end();
-  });
-});
-
-route.for('GET', '/file', function(req, res) {
-  serveStatic(res, 'index.html');
-});
-
 function onRequest(req, res) {
   var pathname = url.parse(req.url).pathname;
   console.log(req.method, req.url, pathname);
@@ -53,8 +30,35 @@ var serveStatic = function(res, file) {
   stream.pipe(res);
 }
 
-http.createServer(onRequest).listen(3000);
-console.log("Server started");
+var skipToQueryReq = function(string) {
+  var first = string.search(/\?/);
+  if(first == -1) return "";
+  return string.slice(first + 1);
+}
 
-console.log(qs.stringify({foo:'bar','baz':[1,2,3]}));
-console.log(qs.parse('q=busqueda&tipo=2'));
+/*
+ * An unique route to handle a specific query
+ * pathURL : /search?q=keyword -> Should be a `q`
+ * query   : q=keyword
+ * queryObj: {q: keyword}
+ */
+route.for('GET', '/search', function(req, res) {
+  var pathURL = req.url;
+  var query = skipToQueryReq(pathURL);
+  var queryObj = qs.parse(query);
+  console.log("--->", query);
+  console.log("--->", queryObj);
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.write("Halo");
+  res.end();
+});
+
+/*
+ * Temporal route(code proofs)
+ */
+route.for('GET', '/aux', function(req, res) {
+  serveStatic(res, 'index.html');
+});
+
+http.createServer(onRequest).listen(3000);
+console.log("Server started on port 3000");
